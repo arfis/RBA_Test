527c2b7b0e1282a883a2757b0cb9c5a358e8a4be
michal.sevcik@atos.net <michal.sevcik@atos.net>
2017-02-08 15:37:39 +0100

    Observables

--- src/time/time.component.ts
+++ src/time/time.component.ts
@@ -3,6 +3,7 @@
  */
 import {Component} from "@angular/core";
 import {TimeService} from "./time.service";
+import {Observable} from "rxjs";
 @Component({
   selector: 'timeComponent',
   templateUrl: './time.component.html'
@@ -14,6 +15,41 @@ export class TimeComponent {
 
   constructor(private service : TimeService){
     this.times = service.getTimes();
+
+    let obs = new Observable(observer => {
+
+      console.log('xxx');
+
+      observer.next({user: {name:'user1'}});
+      observer.next({user: {name:'user2'}});
+      observer.complete();
+
+    }).map((user : any) => {
+
+      return user.user.name;
+
+    })
+      .filter(user => (user == "user2"));
+
+    let p = obs.toPromise();
+
+    p.then(user => {
+
+      console.log('prr');
+      console.log(user);
+    });
+
+    obs.subscribe(user => {
+
+      console.log('obs1');
+      console.log(user);
+    });
+
+    obs.subscribe(user => {
+
+      console.log('obs2');
+      console.log(user);
+    });
   }
 
   timeMessage = 'This is the time component!';
